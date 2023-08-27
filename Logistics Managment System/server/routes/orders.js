8343479bcd07e783b587968ebc5cd7f9585a7bb0
Snookey22 <136700842+Snookey22@users.noreply.github.com>
2023-08-27 15:48:18 +0530

    Update orders.js

--- Logistics Managment System/server/routes/orders.js
+++ Logistics Managment System/server/routes/orders.js
@@ -2,16 +2,16 @@ const express = require('express');
 const router = express.Router();
 const Order = require('../models/order');
 
-// Place a new order
+//  This is to Place a new order
 router.post('/', async (req, res) => {
     try {
-        // Implement order placement logic here
+        // This is to Implement order placement logic here
     } catch (error) {
         console.error(error);
         res.status(500).json({ error: 'Internal Server Error' });
     }
 });
 
-// Implement other CRUD routes for orders
+
 
 module.exports = router;
