c10cd825011b05c0dedb3d351a70f47894ca0507
Snookey22 <136700842+Snookey22@users.noreply.github.com>
2023-08-27 20:29:57 +0530

    Update Hard-Catch the fish.js

--- Syook/syook_answers-Mohamed-Anas/problems/Hard-Catch the fish.js
+++ Syook/syook_answers-Mohamed-Anas/problems/Hard-Catch the fish.js
@@ -1,64 +1,56 @@
-const readline = require('readline');
+let k = 5;
+let l = 2;
+let m = 3;
+let n = 4;
+let total = 24;
+let arr = [];
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
-rl.question('', (input) => {
-  const lines = input.trim().split('\n');
-  const [k, l, m, n, total] = lines[0].split(' ').map(Number);
-  const arr = [];
-
-  for (let i = k; i <= total; i += k) {
-    let f = true;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === i) {
-        f = false;
-      }
-    }
-    if (f) {
-      arr.push(i);
+for (let i = k; i <= total; i += k) {
+  let f = true;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] === i) {
+      f = false;
     }
   }
+  if (f) {
+    arr.push(i);
+  }
+}
 
-  for (let i = l; i <= total; i += l) {
-    let f = true;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === i) {
-        f = false;
-      }
-    }
-    if (f) {
-      arr.push(i);
+for (let i = l; i <= total; i += l) {
+  let f = true;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] === i) {
+      f = false;
     }
   }
+  if (f) {
+    arr.push(i);
+  }
+}
 
-  for (let i = m; i <= total; i += m) {
-    let f = true;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === i) {
-        f = false;
-      }
-    }
-    if (f) {
-      arr.push(i);
+for (let i = m; i <= total; i += m) {
+  let f = true;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] === i) {
+      f = false;
     }
   }
+  if (f) {
+    arr.push(i);
+  }
+}
 
-  for (let i = n; i <= total; i += n) {
-    let f = true;
-    for (let j = 0; j < arr.length; j++) {
-      if (arr[j] === i) {
-        f = false;
-      }
-    }
-    if (f) {
-      arr.push(i);
+for (let i = n; i <= total; i += n) {
+  let f = true;
+  for (let j = 0; j < arr.length; j++) {
+    if (arr[j] === i) {
+      f = false;
     }
   }
+  if (f) {
+    arr.push(i);
+  }
+}
 
-  console.log(arr.length);
-  
-  rl.close();
-});
+console.log(arr.length);
